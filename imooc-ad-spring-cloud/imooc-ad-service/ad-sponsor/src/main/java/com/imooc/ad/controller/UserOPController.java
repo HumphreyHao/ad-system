aa89cb1ed7af373b327a4ce0f5cc751534554f1e
humphreyHao <haz013@ucsd.edu>
2020-02-12 22:28:31 -0800

    search

--- imooc-ad-spring-cloud/imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/controller/UserOPController.java
+++ imooc-ad-spring-cloud/imooc-ad-service/ad-sponsor/src/main/java/com/imooc/ad/controller/UserOPController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Slf4j
 @RestController
-public class UserOPController {
+public class  UserOPController {
 
     private final IUserService userService;
 
